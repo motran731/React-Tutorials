a11f2fe3c9beee5dedd80ef3e112b13bca377a8c
Mo <rxcod3r731@gmail.com>
2024-03-29 13:21:55 -0400

    Added components Card.jsx

---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <div className="card-img">
+          <img className="circle-img" src={props.img} alt="avatar_img" />
+        </div>
+      </div>
+
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
