4417bfb3281096854d487e87a7ab6ad8b22afc7e
Mo <rxcod3r731@gmail.com>
2024-04-01 15:05:04 -0400

    Used ternary operator for login

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import Login from "./Login";
 
 var isLoggedIn = true;
 
-function renderConditionally() {
+/*function renderConditionally() {
   if (isLoggedIn === true) {
     return <h1>Hello</h1>;
   } else {
-    return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
+    return;
+    <Login />;
   }
-}
+} */
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+  );
 }
 
 export default App;
