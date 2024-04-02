96088bf07c67d57302f38d447f082e9366d565e7
Mo <rxcod3r731@gmail.com>
2024-04-02 13:06:48 -0400

    Added useState function to update time. Changed time to 24 hours

--- useState-hook-updateTime/src/components/App.jsx
+++ useState-hook-updateTime/src/components/App.jsx
@@ -3,16 +3,18 @@ import React, { useState } from "react";
 function App() {
   //let time = new Date().toLocaleTimeString();
 
-  const now = new Date().toLocaleTimeString();
+  const now = new Date().toLocaleTimeString("en-US", { hour12: false });
+
+  const [time, setTIme] = useState(now);
 
   function updateTime() {
-    //const newTime = new Date().toLocaleTimeString();
-    console.log("click");
+    const newTime = new Date().toLocaleTimeString("en-US", { hour12: false });
+    setTimeout(newTime);
   }
 
   return (
     <div className="container">
-      <h1>{now}</h1>
+      <h1>{time}</h1>
       <button onClick={updateTime}>Get Time</button>
     </div>
   );
