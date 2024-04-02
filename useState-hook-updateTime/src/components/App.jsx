7ebe2e1717c8a31b7ed1424038f8098972971278
Mo <rxcod3r731@gmail.com>
2024-04-02 13:02:40 -0400

    Comment out time in index.jsx, created a function updateTime in App

--- useState-hook-updateTime/src/components/App.jsx
+++ useState-hook-updateTime/src/components/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //let time = new Date().toLocaleTimeString();
+
+  const now = new Date().toLocaleTimeString();
+
+  function updateTime() {
+    //const newTime = new Date().toLocaleTimeString();
+    console.log("click");
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{now}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
