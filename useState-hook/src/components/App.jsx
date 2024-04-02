86e82fee5cf80dfd8f6ce78462051aa90d2cb511
Mo <rxcod3r731@gmail.com>
2024-04-02 10:43:51 -0400

    Practice with useState, created two functions, decrease and square

--- useState-hook/src/components/App.jsx
+++ useState-hook/src/components/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  function square() {
+    setCount(count * count);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <button onClick={square}> ^2 </button>
+    </div>
+  );
 }
 
 export default App;
