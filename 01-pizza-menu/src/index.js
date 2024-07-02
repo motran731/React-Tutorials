1983d98afa4ca94ca9736a1faf2a0b87d2e87163
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-02 17:52:29 -0400

    Added header, menu, footer, and opening hours

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -6,8 +6,9 @@ function App() {
   return (
     <div>
       <h1> Hello Midori</h1>
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -20,6 +21,34 @@ function Pizza() {
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
+}
+
+function Header() {
+  return <h1> Fast React Pizza Co</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2> Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  //   if (hour >= openHour && hour <= closeHour) alert("We are currently OPEN!");
+  //   else alert("Sorry we are CLOSED");
+
+  return <footer> {new Date().toLocaleTimeString()}, We are OPEN</footer>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
