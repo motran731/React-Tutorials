15a3cbd7dadd64b0e3c492d6fe2d2af4d614d8ca
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-03 15:09:15 -0400

    added css style and working on props

--- 01-pizza-menu/src/index.js
+++ 01-pizza-menu/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 //import pizzaData from "../public/data";
 
 function App() {
   return (
-    <div>
-      <h1> Hello Midori</h1>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -13,31 +13,40 @@ function App() {
   );
 }
 
+function Header() {
+  //const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}> Fast React Pizza Co</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2> Our menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredient="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+      />
+    </main>
+  );
+}
+
 function Pizza() {
   return (
     <div>
-      <h2> Pizza Spinaci</h2>
+      <h3> Pizza Spinaci</h3>
       <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
 }
-
-function Header() {
-  return <h1> Fast React Pizza Co</h1>;
-}
-
-function Menu() {
-  return (
-    <div>
-      <h2> Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
@@ -48,7 +57,11 @@ function Footer() {
   //   if (hour >= openHour && hour <= closeHour) alert("We are currently OPEN!");
   //   else alert("Sorry we are CLOSED");
 
-  return <footer> {new Date().toLocaleTimeString()}, We are OPEN</footer>;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}, We are OPEN
+    </footer>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
