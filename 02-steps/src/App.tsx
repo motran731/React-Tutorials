bcdbf0c1cd76789d82e84ab32a2ade0dc0ddeece
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-09 11:37:35 -0400

    changed steps to a component

--- 02-steps/src/App.tsx
+++ 02-steps/src/App.tsx
@@ -6,6 +6,15 @@ const messages = [
   "Invest your new income 🤑",
 ];
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -22,7 +31,7 @@ export default function App() {
     }
   }
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((isOpEl) => !isOpEl)}>
         &times;
       </button>
@@ -54,6 +63,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
