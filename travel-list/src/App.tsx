e79e3ceaa70a76642965947dc4e66b1cb8557954
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-10 18:23:45 -0400

    updated form cp with array 1 to 20

--- travel-list/src/App.tsx
+++ travel-list/src/App.tsx
@@ -3,6 +3,12 @@
 // import viteLogo from "/vite.svg";
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -19,17 +25,45 @@ function Logo() {
 }
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3> What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button> Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list"> List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description}{" "}
+      </span>
+      <button> ❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em> 🧳 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
