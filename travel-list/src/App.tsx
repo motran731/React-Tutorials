b2bcf7b3467fa951eb2abe1bee92cdaa614caf21
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-10 18:55:13 -0400

    controlled elements for description and quantity. added const newItem

--- travel-list/src/App.tsx
+++ travel-list/src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./index.css";
 
 const initialItems = [
@@ -24,22 +25,35 @@ function Logo() {
   return <h1> Far Away 🌴🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e: any) {
     e.preventDefault();
-    console.log("click");
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3> What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button> Add</button>
     </form>
   );
