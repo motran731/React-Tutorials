651bccaeace2e64b052755dc117d28e66f8eba7c
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-10 17:59:52 -0400

    added travel-list project

---
+++ travel-list/src/App.tsx
@@ -0,0 +1,36 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> Far Away 🌴🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3> What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list"> List</div>;
+}
+function Stats() {
+  return (
+    <footer>
+      <em> 🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
