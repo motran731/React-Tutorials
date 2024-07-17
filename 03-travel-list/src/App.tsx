72063bb04874d9caaaa19903a44cbacbe78373ad
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-17 17:28:48 -0400

    turning functions into components

--- 03-travel-list/src/App.tsx
+++ 03-travel-list/src/App.tsx
@@ -3,7 +3,8 @@
 // import viteLogo from "/vite.svg";
 import { useState } from "react";
 import "./index.css";
-
+import Logo from "./Logo";
+import Form from "./Form";
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
 //   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -52,48 +53,6 @@ export default function App() {
   );
 }
 
-function Logo() {
-  return <h1> Far Away 🌴🧳</h1>;
-}
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e: any) {
-    e.preventDefault();
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-
-    onAddItems(newItem);
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3> What do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button> Add</button>
-    </form>
-  );
-}
 function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
