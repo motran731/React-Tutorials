c79d2704ba153fee148c0beaec5e9d3ce4838438
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-19 15:50:21 -0400

    reuseable button with children prop

--- 02-steps/src/App.tsx
+++ 02-steps/src/App.tsx
@@ -49,19 +49,15 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              text="Previous"
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>🥸 </span> <span>👈 </span> Previous
+            </Button>
 
-            <Button
-              text="Next"
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handleNext}
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              {" "}
+              Next <span>👉 </span>
+              <span>🤓 </span>
+            </Button>
           </div>
         </div>
       )}
@@ -69,11 +65,13 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
-    ></button>
+    >
+      {children}
+    </button>
   );
 }
