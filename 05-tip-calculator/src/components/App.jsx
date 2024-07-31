61531e6514657dc5d6fa4949a3f927e81c953ebe
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-31 14:41:11 -0400

    usestate for bill and percent

--- 05-tip-calculator/src/components/App.jsx
+++ 05-tip-calculator/src/components/App.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const [bill, setBill] = useState();
-
-  // function handleBill(num) {
-  //   setBill(bill);
-  // }
   return (
     <div className="app">
       <TipCalculator />
@@ -14,27 +9,36 @@ export default function App() {
 }
 
 function TipCalculator() {
+  const [bill, setBill] = useState();
+  const [percent1, setPercent1] = useState(0);
+  const [percent2, setPercent2] = useState(0);
   return (
     <div>
-      <Bill />
+      <Bill bill={bill} setBill={setBill} />
 
-      <Percentage> How did you like the service?</Percentage>
+      <Percentage percent1={percent1} setPercent1={setPercent1}>
+        {" "}
+        How did you like the service?
+      </Percentage>
 
-      <Percentage>How did your friend like the service?</Percentage>
+      <Percentage percent2={percent2} setPercent2={setPercent2}>
+        How did your friend like the service?
+      </Percentage>
 
-      <Output />
+      <Output bill={bill} />
       <Reset />
     </div>
   );
 }
-function Bill() {
+function Bill({ bill, setBill }) {
   return (
     <div>
       <label>How much was the bill?</label>
       <input
         type="text"
         placeholder="Bill value"
-        // onChange={(e) => setBill(e.target.value)}
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
       />
     </div>
   );
@@ -44,18 +48,18 @@ function Percentage({ children }) {
   return (
     <div>
       <label> {children} </label>
-      <select className="percentage">
+      <select className="percentage" value="" onChange="">
         <option value="0"> Dissatisfied (0%)</option>
         <option value="5"> It was okay (10%)</option>
-        <option value="10%"> It was good (15%)</option>
-        <option value="20%"> Absolutely amazing! (20%)</option>
+        <option value="10"> It was good (15%)</option>
+        <option value="20"> Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
 }
 
-function Output() {
-  return <h2> You pay $X ($tip1+ $tip2)</h2>;
+function Output({ bill }) {
+  return <h2> Your pay $X (${bill}+ $tip2)</h2>;
 }
 
 function Reset() {
