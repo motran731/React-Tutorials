99b732e39c1a3a5042df94cde440d2408512b93a
Midori <midori@Midoris-MacBook-Pro.local>
2024-07-31 11:38:59 -0400

    changed crcl calculator to tip calculator

---
+++ 05-tip-calculator/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <form className="tip">
+        <h3> How much was the bill</h3>
+        <h3> How did you like the service</h3>
+        <h3> How did yourr friend like the service?</h3>
+        <h2> You pay $100 ($80 +12tip)</h2>
+        <button> Reset</button>
+      </form>
+    </div>
+  );
+}
