7d66eab642aa3e84f4b5ab0bae71c3b850a0287f
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-02 14:23:56 -0400

    friends list and balance

--- 06-split-calculator/src/App.jsx
+++ 06-split-calculator/src/App.jsx
@@ -45,5 +45,24 @@ function FriendList() {
 }
 
 function Friend({ friend }) {
-  return <li> {friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3> {friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.anme} {Math.abs(friend.balance)} $
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)} $
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You owe {friend.anme} are even</p>}
+      <button className="button"> Select </button>
+    </li>
+  );
 }
