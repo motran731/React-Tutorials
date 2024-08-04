5b5709e75aefdc9645a8a5f6d1b779eef8057e37
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-04 12:32:06 -0400

    static layout. spill bill form added

--- 06-split-calculator/src/App.jsx
+++ 06-split-calculator/src/App.jsx
@@ -31,6 +31,7 @@ export default function App() {
         <FormAddFriend />
         <Button> Add friend </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -76,12 +77,35 @@ function Button({ children }) {
 //NOTES building static
 function FormAddFriend() {
   return (
-    <form className="form -add-friend">
+    <form className="form-add-friend">
       <label> 👫 Friend name </label>
       <input type="text" />
 
       <label> 🌇 Image URL </label>
       <input type="text" />
+
+      <Button> Add </Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2> Split a bill with X </h2>
+      <label> 💰 Bill value</label>
+      <input type="text" />
+
+      <label> 🙆🏻‍♂️ Your Expense</label>
+      <input type="text" />
+
+      <label> 👫 X's expense</label>
+      <input type="text" disabled />
+      <label> 🤑 Who is paying the bill</label>
+      <select className="select">
+        <option value="user"> You</option>
+        <option value="friend"> X </option>
+      </select>
 
       <Button> Add </Button>
     </form>
