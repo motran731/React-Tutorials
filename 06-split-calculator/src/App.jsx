196a2b69eb44a33d7fcb06b9fa3ac8f412693d0a
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-04 14:36:58 -0400

    calculates the split bill

--- 06-split-calculator/src/App.jsx
+++ 06-split-calculator/src/App.jsx
@@ -51,6 +51,17 @@ export default function App() {
     setShowAddFriend(false);
   }
 
+  function handleSplitBill(value) {
+    console.log(value);
+    setFriends((friend) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -67,7 +78,12 @@ export default function App() {
         </Button>
       </div>
 
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
@@ -164,14 +180,22 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
   const paidByFriend = bill ? bill - paidByUser : "";
 
   const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2> Split a bill with {selectedFriend.name} </h2>
       <label> 💰 Bill value</label>
       <input
@@ -202,7 +226,7 @@ function FormSplitBill({ selectedFriend }) {
         <option value="friend"> {selectedFriend.name} </option>
       </select>
 
-      <Button> Add </Button>
+      <Button> Split bill </Button>
     </form>
   );
 }
