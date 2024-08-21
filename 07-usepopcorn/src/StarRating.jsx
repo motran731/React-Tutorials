25fa951b99108fb4a2576437876af9f202b5c512
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-21 15:22:33 -0400

    hover or click on empty stars, it will turn full

--- 07-usepopcorn/src/StarRating.jsx
+++ 07-usepopcorn/src/StarRating.jsx
@@ -32,11 +32,11 @@ export default function StarRating({ maxRating = 5 }) {
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
           />
         ))}
       </div>
-      <p style={textStyle}> {rating || ""} </p>
+      <p style={textStyle}> {tempRating || rating || ""} </p>
     </div>
   );
 }
