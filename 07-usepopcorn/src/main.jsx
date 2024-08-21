d703dd9d041904ec31ddcd4bc044d680b28eb5b1
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-21 14:59:10 -0400

    reusable star rating, star image

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
 
-createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
+  </StrictMode>
+);
