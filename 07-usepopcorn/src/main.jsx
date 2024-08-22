b99606096d4e05a7ac88337e0aa5c0473ec8a69e
Midori <midori@Midoris-MacBook-Pro.local>
2024-08-22 18:26:39 -0400

    test cp to use the internal rating externally

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import App from './App.jsx'
 // import './index.css'
 
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p> This movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -13,5 +24,7 @@ createRoot(document.getElementById("root")).render(
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="text" defaultRating={3} />
+
+    <Test />
   </StrictMode>
 );
